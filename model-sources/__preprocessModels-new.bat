6b59d581072b7e177b90f39f32ef3e17defd718d
Giorgio Cangioli <35879607+gcangioli@users.noreply.github.com>
2022-11-13 22:22:19 +0100

    https://github.com/hl7-eu/pcsp/issues/56 new relapseAfterFlt.fsh

---
+++ model-sources/__preprocessModels-new.bat
@@ -0,0 +1,19 @@
+
+for /R ..\input\fsh\models-new %%f in (*.fsh) do (
+	echo %%f
+ CALL :processFshFile %%f  
+)
+
+
+:processFshFile
+
+ fart %~1  \"Description: Description:
+ fart %~1  \"Title: Title:
+ fart %~1  "\"* " "* "
+ fart %~1  ##334#\" \"\"\"
+ fart %~1  ##334# \"\"\"
+ fart %~1  ##34# \" 
+ 
+ GOTO :EOF
+
+
